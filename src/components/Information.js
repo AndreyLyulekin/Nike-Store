13fb82be57f9b3c31087ce07265962e3881693a8
Andrey Lyulekin <lyulekin@gmail.com>
2023-11-21 21:33:39 -0300

    bug fixing

--- src/components/Information.js
+++ src/components/Information.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import shoppingBag from '../images/ShoppingBag.png';
 import CircleBtn from './CircleBtn';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import FormHelperText from '@mui/material/FormHelperText';
 
 export default function Information() {
   const [size, setSize] = React.useState('');
@@ -21,41 +15,29 @@ export default function Information() {
       <div className='productInfo__size-block'>
         <p className='productInfo__price'>$275</p>
         <div className='productInfo__size-right-block'>
-          <Box sx={{ minWidth: 150, paddingTop: '15px' }}>
-            <FormControl
-              fullWidth
-              variant='filled'>
-              <InputLabel
-                id='demo-simple-select-label'
-                sx={{ color: 'whitesmoke', fontSize: '24px', fontFamily: 'Futura Bk BT' }}>
-                Choose Size
-              </InputLabel>
-              <Select
-                labelId='demo-simple-select-label'
-                id='demo-simple-select'
-                value={size}
-                label='Size'
-                sx={{ color: 'whitesmoke', backgroundColor: 'black', fontSize: '24px' }}
-                onChange={handleChange}>
-                <MenuItem
-                  value={40}
-                  sx={{ color: 'whitesmoke', backgroundColor: 'black', fontSize: '24px' }}>
-                  40
-                </MenuItem>
-                <MenuItem
-                  value={41}
-                  sx={{ color: 'whitesmoke', backgroundColor: 'black', fontSize: '24px' }}>
-                  41
-                </MenuItem>
-                <MenuItem
-                  value={42}
-                  sx={{ color: 'whitesmoke', backgroundColor: 'black', fontSize: '24px' }}>
-                  42
-                </MenuItem>
-              </Select>
-              <FormHelperText sx={{ color: 'white', userSelect: 'none' }}>Кликни</FormHelperText>
-            </FormControl>
-          </Box>
+          <div className='select__wrapper'>
+            <select
+              onClick={handleChange}
+              name='city1'
+              className='select'>
+              <option value=''>Size</option>
+              <option
+                value='petersburg'
+                selected=''
+                className='option'>
+                Санкт-Петербург
+              </option>
+              <option value='moscow'>Москва</option>
+              <option value='kazan'>Казань</option>
+              <option
+                value='samara'
+                disabled=''>
+                Самара
+              </option>
+              <option value='perm'>Пермь</option>
+              <option value='novosibirsk'>Новосибирск</option>
+            </select>
+          </div>
           <img
             src={shoppingBag}
             alt='ShoppingBag-Icon'
